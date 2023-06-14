fa65d409eb40b2dce851c763645c78e94c9a0712
simon <1311280323@qq.com>
2023-06-15 00:20:12 +0800

    [lc:arr]643 error

--- python/id1arr.py
+++ python/id1arr.py
@@ -1,4 +1,27 @@
-import paddle 
+from typing import List
 
 
-print()
+class Solution643: # ERROR
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        n = len(nums)
+        sum_ = sum(nums[:k])
+        ans = sum_
+        
+        for i in range(k, n): 
+            sum_ = ans + nums[i] - nums[i - k]
+            if sum_ > ans:
+                ans = sum_ 
+
+        return ans / k
+
+
+def main():
+    so = Solution643()
+    nums = [1,12,-5,-6,50,3]
+    k = 4
+    res = so.findMaxAverage(nums, k)
+    print(res)
+    
+    
+if __name__ == '__main__':
+    main()
